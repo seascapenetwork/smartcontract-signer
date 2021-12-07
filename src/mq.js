022031710614590b32ba162dccc402e5361d4ede
ahmetson <ahmetson@zoho.com>
2021-12-07 19:40:25 +0700

    Add RPC send reply

--- src/mq.js
+++ src/mq.js
@@ -5,11 +5,24 @@ let mqUrl = `amqp://${process.env.MQ_HOST || 'mq'}`;
 let QUEUE_TYPE = {
     GATEWAY: {
         queue: 'gateway_queue',
-        exclusive: false
+        exclusive: false,
+        durable: true,
+        noack: false,
+        autoDelete: true
     },
     SIGNER: {
         queue: 'signer_queue',
-        exclusive: false
+        exclusive: false,
+        durable: true,
+        noack: false,
+        autoDelete: true
+    },
+    RPC: {
+        queue: '',
+        exclusive: true,
+        durable: false,
+        noack: true,
+        autoDelete: true
     }
 }
 
@@ -39,12 +52,17 @@ let connect = async () => {
  * @param {boolean} exclusive only one queue consumer?
  */
 let attachToQueue = async (channel, queueType) => { 
-    await channel.assertQueue(queueType.queue, {
-        durable: true,
-        exclusive: queueType.exclusive
+    let res = await channel.assertQueue(queueType.queue, {
+        durable: queueType.durable,
+        exclusive: queueType.exclusive,
+        autoDelete: queueType.autoDelete
     });
     
     await channel.prefetch(1);
+
+    if (queueType.exclusive) {
+        return res;
+    }
 }
 
 let disconnect = async(connection, channel) => {
@@ -69,27 +87,32 @@ let listenQueue = async (channel, queueType, onMsg) => {
             return channel.reject(msg, false);
         }
     
-        let res = await onMsg(content);
+        let res = await onMsg(content, msg.properties.replyTo, msg.properties.correlationId);
         if (res === false) {
-            channel.reject(msg, false);
+            if (!queueType.noack) {
+                channel.reject(msg, false);
+            }
         } else if (res === true) {
-            channel.ack(msg);
+            if (!queueType.noack) {
+                channel.ack(msg);
+            }
         } else {
             throw `Invalid response from onMsg callback for queue ${queueType.queue}. Expected either true or false`;
         }
     }, {
         // manual acknowledgment mode,
         // see ../confirms.html for details
-        noAck: false
+        noAck: queueType.noack
     });
 };
 
-let sendToQueue = async (channel, queueType, obj) => {
+let sendToQueue = async (channel, queueType, obj, options) => {
     let str = JSON.stringify(obj);
-    return channel.sendToQueue(queueType.queue, Buffer.from(str));
+    console.log(`Send signal to ${queueType.queue}, with data `, obj, options);
+    return channel.sendToQueue(queueType.queue, Buffer.from(str), options);
 };
 
-let sendOverMq = async (queueType, data) => {
+let sendOverMq = async (queueType, data, options) => {
     let conChannel;
     try {
         conChannel = await connect();
@@ -100,7 +123,7 @@ let sendOverMq = async (queueType, data) => {
     try {
         await attachToQueue(conChannel.channel, queueType);
     } catch (error) {
-        return console.error(chalk.red(`Internal error. Could not register or create ${mq.QUEUE_TYPE.GATEWAY.queue} queue`));
+        return console.error(chalk.red(`Internal error. Could not register or create ${queueType.queue} queue`));
     }
 
     try {
@@ -118,6 +141,59 @@ let sendOverMq = async (queueType, data) => {
     return true;
 };
 
+
+let sendOverRpc = async (queueType, data, callback) => {
+    let conChannel;
+    try {
+        conChannel = await connect();
+    } catch (error) {
+        return console.error(chalk.red(`Internal error. Could not connect to the Message Queue`));
+    }
+
+    let attachment;
+    try {
+        attachment = await attachToQueue(conChannel.channel, QUEUE_TYPE.RPC);
+    } catch (error) {
+        return console.error(chalk.red(`Internal error. Could not register or create ${queueType.queue} queue`));
+    }
+
+    var correlationId = generateUuid();
+
+    let rpcQueueType = QUEUE_TYPE.RPC;
+    rpcQueueType.queue = attachment.queue;
+
+    await listenQueue(conChannel.channel, rpcQueueType, async (content, _replyTo, _correlationId) => {
+        if (_correlationId != correlationId) {
+            return false;
+        }
+
+        try {
+            await disconnect(conChannel.connection, conChannel.channel);
+        } catch (error) {
+            console.error(chalk.redBright(`Failed to disconnect from Message Queue. Please do it manually`));
+            return false;
+        }
+
+        callback(content);
+
+        return true;
+    });
+
+    try {
+        await sendToQueue(conChannel.channel, queueType, data, {replyTo: rpcQueueType.queue, correlationId: correlationId});
+    } catch (error) {
+        return console.error(chalk.redBright(`Failed to send the data over RPC Message Queue for ${queueType.queue}`));
+    }
+
+    return true;
+};
+
+function generateUuid() {
+    return Math.random().toString() +
+           Math.random().toString() +
+           Math.random().toString();
+}
+
 module.exports = {
     QUEUE_TYPE,
     connect,
@@ -125,5 +201,6 @@ module.exports = {
     attachToQueue,
     listenQueue,
     sendToQueue,
-    sendOverMq
+    sendOverMq,
+    sendOverRpc
 }
